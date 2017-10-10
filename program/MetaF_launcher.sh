d3c74c2145bee3e834b8d5ec296e19f196878980
Jean Mainguy <jean.mainguy@etu.u-bordeaux.fr>
2017-10-11 01:54:54 +0200

    add hmmsearch line in te launcher

--- program/MetaF_launcher.sh
+++ program/MetaF_launcher.sh
@@ -15,6 +15,14 @@ dependency_pathway=/home/jean/Documents/Master_Bioinfo/S2_Bilbao/StageBAO/MeTAfi
 mkdir ${output_pathway}
 
 
+#HMMSEARCH parameter below should normally not be changed
+#HMM result table python script expects : table_hmm = output_way + '/' + metaG_name + '_output_tableHMM.txt'
+table_hmm=${output_pathway}/${MetaG_name}_output_tableHMM.txt
+HMM_DB=${dependency_pathway}/ALL_plus_MET_curatted.hmm
+faa_data=${data_pathway}/${data_name}.faa
+
+hmmsearch -E 0.5 --domtblout ${table_hmm}  ${HMM_DB} ${faa_data} > /dev/null
+
 # usage: MeTAfisher [-h] [--Resize] [--Rescue] [--contig_name CONTIG_NAME]
 #                   [--HMM_db HMM_DB]
 #                   MetaG_name output_pathway data_pathway data_name
@@ -22,3 +30,4 @@ mkdir ${output_pathway}
 
 
 python program/main_MetaF.py --Resize --Rescue ${MetaG_name} ${output_pathway} ${data_pathway} ${data_name} ${dependency_pathway}
+# python program/main_MetaF.py ${MetaG_name} ${output_pathway} ${data_pathway} ${data_name} ${dependency_pathway}
