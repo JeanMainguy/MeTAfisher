fa464f660f19b212955e594d3f9b5b1536086b7c
Jean <jean.mainguy@outlook.fr>
2017-12-15 15:05:47 +0100

    old file replaced by genbank_parser

--- program/genbank_parser.py
+++ program/genbank_parser.py
@@ -23,10 +23,38 @@ def write_faa(feat, faa_fl, chrm, seq, attributes, nbgene):
     trans_table = int(feat.qualifiers['transl_table'][0])
 
 
+    # if 'pseudo' in f.qualifiers:
+    #     print "PSEUDOOOOOOO"
+    # # print attributes
+    try:
+        seqtranslate = seq.translate(table=trans_table, to_stop=True, cds=True)
+    except:
+        # print f.qualifiers
+        print attributes
+        print 'PROBLEM with translation'
+        seqtranslate =  False
+
     if 'translation' in f.qualifiers:
         seqAA = Seq(feat.qualifiers['translation'][0])
+
+        if  str(seqAA)[1:] != str(seqtranslate)[1:]:
+            print 'Sequence from gb file and translate fct are different'
+            print 'from qualifiers ', str(seqAA)
+            print 'translate :', str(seqtranslate)
+            # raw_input()
+        # else:
+        #     print 'Pareil'
     else:
-        seqAA = seq.translate(table=trans_table, to_stop=True)
+        print 'no translation in gb file'
+        seqAA = seqtranslate
+        print attributes
+        print 'translate :', str(seqtranslate)
+        # raw_input()
+
+    if not seqtranslate:
+        return False
+
+
     # print type(seqAA)
     # print attributes
     record = SeqRecord(seqAA, id="{}|{} ".format(chrm,str(nbgene)), name="", description=' '.join(attributes))
@@ -48,34 +76,34 @@ def see_objet(obj):
             print attr, " ", getattr(obj, attr)
 
 
+if __name__ == '__main__':
+    gk_file = sys.argv[1] # 'data/Acaryochloris_marina_MBIC11017/sequence.gb'
+    data_way = gk_file.split(".gb")[0] #'data/Acaryochloris_marina_MBIC11017/'
+    print data_way
+    #file_name = "sequence"
 
-gk_file = sys.argv[1] # 'data/Acaryochloris_marina_MBIC11017/sequence.gb'
-data_way = gk_file.split(".gb")[0] #'data/Acaryochloris_marina_MBIC11017/'
-print data_way
-#file_name = "sequence"
+    gff_fl = open(data_way +'.gff', 'w')
+    faa_fl = open(data_way + '.faa', 'w')
+    fna_fl = open(data_way + '.fna', 'w')
 
-gff_fl = open(data_way +'.gff', 'w')
-faa_fl = open(data_way + '.faa', 'w')
-fna_fl = open(data_way + '.fna', 'w')
+    input_handle = open(gk_file, "rU")
+    for record in SeqIO.parse(input_handle, "genbank"):
+        # see_objet(record)
+        SeqIO.write(record, data_way+".fasta", "fasta")
+        i=0
+        c = 0
+        for f in record.features:
+            # if i > 150:
+            #     break
+            if f.type == 'CDS' : #and 'translation' in f.qualifiers:
+                i += 1
+                seq = f.extract(record.seq)
+                attributes = giveQualifiersInfo(f.qualifiers, ["old_locus_tag", 'protein_id', "locus_tag",'product'])
+                if write_faa(f, faa_fl, record.id, seq,  attributes,i) is not False:
+                    write_gff(f,gff_fl, record.id, attributes, i)
+                    write_fna(f, fna_fl, record.id, seq, attributes, i)
 
-input_handle = open(gk_file, "rU")
-for record in SeqIO.parse(input_handle, "genbank"):
-    # see_objet(record)
-    SeqIO.write(record, data_way+".fasta", "fasta")
-    i=0
-    c = 0
-    for f in record.features:
-        # if i > 150:
-        #     break
-        if f.type == 'CDS' : #and 'translation' in f.qualifiers:
-            i += 1
-            seq = f.extract(record.seq)
-            attributes = giveQualifiersInfo(f.qualifiers, ["old_locus_tag", 'protein_id', "locus_tag",'product'])
-            write_gff(f,gff_fl, record.id, attributes, i)
-            write_faa(f, faa_fl, record.id, seq,  attributes,i)
-            write_fna(f, fna_fl, record.id, seq, attributes, i)
-
-#             if 'tox' in f.qualifiers['product'][0]:
-#                 print f.qualifiers
-#                 c += 1
-# print c
+    #             if 'tox' in f.qualifiers['product'][0]:
+    #                 print f.qualifiers
+    #                 c += 1
+    # print c
--- program/hmm_adjorf.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# echo 'START HMM'
-hmmdb=dependence/ALL_plus_MET_curatted.hmm
-seqfile=data/temporary_adjOrf.faa
-out_table=./data/table_hmm.txt
-# echo HMMSEARCH
-hmmsearch -E 0.5 -o tmp.txt  --domtblout ${out_table}  ${hmmdb} ${seqfile}
-# echo 'END HMM'
