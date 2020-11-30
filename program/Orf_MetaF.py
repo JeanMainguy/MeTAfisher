1e7468f6b93ab27045ff00b99dc70424f9e22a10
Jean Mainguy <jean.mainguy@outlook.fr>
2020-11-30 20:53:16 +0100

    clean orf script

--- program/Orf_MetaF.py
+++ program/Orf_MetaF.py
@@ -1,12 +1,21 @@
-# coding: utf-8
+#!/usr/bin/env python3
+
+"""
+Module      : Orf
+Description : The main entry point for the program.
+Copyright   : (c) Jean Mainguy, 27 nov. 2020
+License     : MIT
+Maintainer  : jean.mainguy@outlook.fr
+
+
+Program to retrieve toxin antitoxin (TA) systems in genomes or metagenomes.
+"""
+
+
 from itertools import chain
 import Object_MetaF as obj
-import Function_MetaF as fct2
-# import re
-# import csv
-# from operator import attrgetter, itemgetter
-# import find_orf as orf
-# from math import log
+import Function_MetaF as fct
+import logging
 
 
 def rescue_lonely_gene(dico_orf, dico_gff, scaffold, tmp_adjorf_faa):
@@ -21,14 +30,13 @@ def rescue_lonely_gene(dico_orf, dico_gff, scaffold, tmp_adjorf_faa):
     """
     fl = open(tmp_adjorf_faa, 'w')
     # Extraction of the sequence of the contig in order to find the orf inside.
-    seq, dico_orf["line"] = fct2.get_fast_fasta(dico_orf['fl'], dico_orf['line'], scaffold)
+    seq, dico_orf["line"] = fct.get_fast_fasta(dico_orf['fl'], dico_orf['line'], scaffold)
     obj.Orf.seq = seq
     # retrieve the end position of the predicted gene to then skip the related ORF:
     gff_ends, highestGeneNumber = get_gff_ends(dico_gff, scaffold)
 
     # TEST IF THERE ARE LONELY GENE IN STRAND plus
     if obj.TA_gene.lonely['+']:
-        # print 'THERE IS LONELY GENE ON STRAND +'
         generator = findORF(scaffold, seq, rev=1)
         # read the generator: check if the orf is a predicted gene
         # then check if the orf is adjacent with a TA_gene
@@ -37,17 +45,13 @@ def rescue_lonely_gene(dico_orf, dico_gff, scaffold, tmp_adjorf_faa):
         orf_manager(generator, '+', obj.TA_gene.genes_strand['+'], gff_ends, fl)
     # TEST IF THERE ARE LONELY GENES IN STRAND minus
     if obj.TA_gene.lonely['-']:
-        # print 'THERE IS LONELY GENE ON STRAND -'
         generator = findORF(scaffold, seq, rev=-1)
         orf_manager(generator, '-', obj.TA_gene.genes_strand['-'], gff_ends, fl)
-    # else:
-        # raw_input(scaffold + " THERE IS NO LONELY GENE ON STRAND -")
     fl.close()
-    # print obj.Orf.adj_orf
     # If the program have found adj orf then several step are applied
     if obj.Orf.adj_orf:
         # launch hmmsearch
-        table_hmm = fct2.HMM_launcher(tmp_adjorf_faa, add_to_name='adj_orf')
+        table_hmm = fct.HMM_launcher(tmp_adjorf_faa, add_to_name='adj_orf')
         # parsing result and store it in obj.Orf.hmm_orf
         adjOrf_HMM(table_hmm)
         # taking into account the domain in the possible start of the ORF
@@ -72,16 +76,14 @@ def hmm_orf_get_adj(dico_hmmorf):
             for gene in obj.TA_gene.genes_strand[strand]:
                 # if the orf have been already process and added to the list then we don't check it against itself
                 if hmmorf.gene_number == gene.gene_number:
-                    # print hmmorf
-                    # print gene
                     continue
+
                 if gene.is_pre_adj_to(hmmorf):
                     add_post_pre_attr(strand, g_post=hmmorf, g_prev=gene)
-                    # print gene, 'is adj to', hmmorf
 
                 elif hmmorf.is_pre_adj_to(gene):
                     add_post_pre_attr(strand, g_post=gene, g_prev=hmmorf)
-                    # print gene, 'is adj to', hmmorf
+
             obj.TA_gene.genes_strand[strand].append(hmmorf)
             obj.TA_gene.genes.append(hmmorf)
 
@@ -106,11 +108,7 @@ def adjust_orf_attribut(dico_obj, highestGeneNumber):
     """
     for strand in dico_obj:
         for o in dico_obj[strand]:
-            # print 'CT BORDER', o.domain_Ct_border
-            # print 'before', o.possible_start
-            # starts_zip = zip(o.possible_start, o.possible_start_orf)
             o.possible_start = [s for s in o.possible_start if s <= o.domain_Ct_border]
-            # print 'after', o.possible_start
             o.distanceMin = obj.Gene.distanceMin - abs(o.possible_start[-1] - o.possible_start[0])
 
             # WARNING absolute value of distance min should not be greater than the length of the gene !!
@@ -140,7 +138,6 @@ def codon_finder(liste, seq, frame=1, inf=0, sup='Not defined'):
     Returns:
         Return a list of the position of the given codon in the sequence
     """
-
     position = []
     if sup == 'Not defined':
         sup = len(seq) - 2
@@ -155,19 +152,11 @@ def orf_manager(generator, strand, lonelyGenes, gffEnds, fl):
     lonely gene are a list of lonely gene from the same strand
     check to see if orf is a predicted gene
     """
-    # print 'STRAND ', strand
-    # print 'GFF ENDS\n', gffEnds[strand]
-    # print lonelyGenes[0]
     for o in generator:
         if o.real_end() in gffEnds[strand]:
-            # print 'Is a predicted gene..', o
             gffEnds[strand].remove(o.real_end())
             continue
-        # resize if too long.. Not possible to have something to small
-        # because orfinder has already a minimum size limit tha fit the threshold
-        # set also the g.distanceMin personalized of the orf to get the tandem gene.
         # EDIT now orfinder size max and min correctly...
-        # o.manage_size()  # A enlever quand tu le sens attention au distanceMin ! !
         o.distanceMin = obj.Gene.distanceMin - (o.possible_start[-1] - o.possible_start[0])
         # WARNING absolute value of distance min should not be greater than the length of the gene !!
         # because it would allow overlap of more than the length of the gene
@@ -180,87 +169,52 @@ def orf_manager(generator, strand, lonelyGenes, gffEnds, fl):
 
             obj.Orf.adj_orf_index += 1
             obj.Orf.adj_orf[obj.Orf.adj_orf_index] = o
-            # print obj.Gene.distanceMin, '-(', (o.possible_start[-1], '-', o.possible_start[0]), ') = ', o.distanceMin
-            # print 'Index', obj.Orf.adj_orf_index, 'distance min', o.distanceMin
-            # try:
-            #     obj.Orf.adj_orf.setdefault(strand, []).append(o)
-            # except AttributeError:
-            #     obj.Orf.adj_orf = {'+': [], '-': []}
-            #     obj.Orf.adj_orf[strand].append(o)
             o.write_faa(fl, index=obj.Orf.adj_orf_index)  # write the file to tmp faa file
-    # ==============================================
-        # #REMOVE##
-        # if o.strand == '+' and o.real_end() > 24000:
-        #     print 'OLALALA'
-        #     print o, 'frame :', o.frame, 'real end', o.real_end()
-        ####
+
     if gffEnds[strand]:
-        print(o.scaffold)
-        print(strand)
-        print('len du saffold', len(o.seq['data']))
-        print("THERE ARE SOME PEDICTED GENE THAt DIDNT FOUND THEIR ORF :-(")
-        # TODO LOG CHANGE THAT
+        logging.warning(o.scaffold)
+        logging.warning(strand)
+        logging.warning(f"len du saffold {len(o.seq['data'])}")
+        logging.warning("THERE ARE SOME PEDICTED GENE THAt DIDNT FOUND THEIR ORF :-(")
+
         with open(obj.Gene.outdir + '/Predicted_not_found.err', 'a') as fl:
             fl.write("scaffold " + o.scaffold + ' strand :' + strand + '\n' + str(gffEnds[strand]))
             for g in gffEnds[strand]:
                 fl.write(str(g) + '\n')
-        # print gffEnds[strand]
-    # if strand == "+":
-    #     with open(obj.Gene.output_way + 'sca_len.txt', 'a') as fl:
-    #         fl.write(o.scaffold + '   ' + str(len(o.seq['data'])) + '\n')
-    #         fl.write(o.seq['data'][len(o.seq['data']) - 20:len(o.seq['data'])])
 
 
 def adjOrf_HMM(table_hmm):
-    """
-
-    """
-
-    # tablehmm = obj.Gene.output_way + '/table_hmm_adjorf.txt'
-    # hmm_db = obj.Gene.hmmdb
-    #
-    # bash_commande = "hmmsearch -E 0.5  --domtblout {}/output/MP0313/test/adjOrf_table_hmm.txt  {}/dependence/ALL_plus_MET_curatted.hmm {}/data/MP0313/MP0313.549.faa".format(home, home, home)
-    # print bash_commande
-    #
-    # call(bash_commande, shell=True)
-    # call(obj.Orf.hmm_adjorf_launcher, shell=True)
-
+    """Do something."""
     with open(table_hmm, 'r') as fl:
         for line in fl:
             if line[0] != "#":
-                # print line
-                gene_number, domain = fct2.hmmtable_parser(line)
+                gene_number, domain = fct.hmmtable_parser(line)
                 hmm_orf = obj.Orf.adj_orf[gene_number]
                 # hmm_orf.gene_number = obj.Gene.highestGeneNumber
 
                 try:
                     hmm_orf.domain.append(domain)
                     hmm_orf.domain_Ct_border = max(hmm_orf.domain_Ct_border, domain.ali_from * 3)
-                except:
+                except AttributeError:
                     hmm_orf.domain_Ct_border = domain.ali_from * 3
                     hmm_orf.domain = [domain]
                     obj.Orf.hmm_orf.setdefault(hmm_orf.strand, []).append(hmm_orf)
 
-                # Append to hmm_orf dico of class Attribute
-
 
 def get_gff_ends(dico_gff, scaffold):
-    '''
+    """
     dico_gff is composed of the line and the fl of the gff file
     Work in the same way as get_fast_fasta
     Yield the gff stop according the frame
     Then return a dictionary composed of the generator by frame
-    '''
+    """
     gff_ends = {'+': [], '-': []}
     line = dico_gff["line"]
-    # print line
     fl = dico_gff["csv"]
     while line and line[0] != scaffold:
         line = next(fl)
-    # for line in fl:
-    while line and line[0] == scaffold:
-        # print line
 
+    while line and line[0] == scaffold:
         if line[0] != scaffold:
             break
         if line[2] not in ['CDS', 'gene']:
@@ -286,7 +240,7 @@ def get_gff_ends(dico_gff, scaffold):
 
 
 def findORF(scaffold, seq, rev):
-    """This function determine the orf of a nucleic sequence
+    """Determine the orf of a nucleic sequence.
     This function call the orf_by_frame() function  for each frame and give it to it different parameter
     especially the frame and the information if the sequence is reverse or not and finally gather the result in a dictionary
 
@@ -298,19 +252,13 @@ def findORF(scaffold, seq, rev):
         A list of ORFs in the class ListGene
 
     """
-
     threshold = obj.Orf.length_min
     starts = obj.Gene.codon_starts
     stops = obj.Gene.codon_stops
-    # print 'IN FIND ORF...'
+
     if (rev == -1 and seq["rev"] is False) or (rev == 1 and seq['rev'] is True):
-        # print 'Pre rev?', seq["rev"], 'rev=', rev
         seq = complement_reverse(seq)
-        # print 'Post rev? ', seq["rev"], 'rev=', rev
-    # generator_dict = {}
-    # for frame in [1, 2, 3]:
-    #     generator_dict[frame * rev] = orf_by_frame(seq, threshold, starts, stops, frame=frame, rev=rev)
-    # return generator_dict
+
     generator1 = orf_by_frame(seq, threshold, starts, stops, frame=1, rev=rev)
     generator2 = orf_by_frame(seq, threshold, starts, stops, frame=2, rev=rev)
     generator3 = orf_by_frame(seq, threshold, starts, stops, frame=3, rev=rev)
@@ -319,7 +267,7 @@ def findORF(scaffold, seq, rev):
 
 
 def orf_by_frame(seq, threshold, starts, stops, frame, rev):
-    """This function find the orf in a sequence for a given frame
+    """Find the orf in a sequence for a given frame.
 
     First the function determine all the stop codon of the sequence in frame by calling codon_finder().
     Then it will find all the start codon flan by two stop codon by calling codon_finder
@@ -335,13 +283,11 @@ def orf_by_frame(seq, threshold, starts, stops, frame, rev):
 
     Returns:
         The list of the Orf of the frame as a list of dictionaries
-
-        """
+    """
     length_seq = len(seq['data'])
     pos_stop = codon_finder(stops, seq['data'], frame=frame)
     if not pos_stop:  # Check the case if there is no stop codon in te sequence
-        # TODO LOG
-        print("NO STOP IN THE SEQUENCE")
+        logging.debug("NO STOP IN THE SEQUENCE")
         if length_seq > threshold:
             twoStopLen = length_seq
             twoStopLen = twoStopLen - twoStopLen % 3
@@ -352,7 +298,7 @@ def orf_by_frame(seq, threshold, starts, stops, frame, rev):
             add_to_inf = to_fit_len_max(twoStopLen=length_seq)
             # research of starts in the whole sequence !
             pos_start = codon_finder(starts, seq['data'], inf=add_to_inf, frame=frame)
-            # ATTENTION ajout du debut de la sequence aux start... start imaginaire donc, mais necessaire pour coller au gene predit ! :-(
+            # WARNING add at the beggining of the seq an imaginary star, necessary to stick to gene prediction
             if add_to_inf == 0:
                 pos_start = [frame - 1] + pos_start
 
@@ -373,15 +319,15 @@ def orf_by_frame(seq, threshold, starts, stops, frame, rev):
         add_to_inf = to_fit_len_max(twoStopLen)  # +3 is to get the len between 0 to the first stop
         pos_start = codon_finder(starts, seq['data'], frame=frame, inf=add_to_inf, sup=(
             pos_stop[0] + 3 - threshold))  # research of starts from 0 to (first stop - thresold)
-        # ATTENTION ajout du debut de la sequence aux start... start imaginaire donc, mais necessaire pour coller au gene predit ! :-(
+
         if add_to_inf == 0:
             pos_start = [frame - 1] + pos_start
         if pos_start:
             yield obj.Orf(frame=frame * rev, possible_start_orf=pos_start, end_orf=pos_stop[0] + 2, complet=True, border=True)
 
-    for i in range(len(pos_stop) - 1):  # recherche entre le stop i et i+1 donc on s'arrete a un stop avant la fin
+    for i in range(len(pos_stop) - 1):  # search between stop i and stop i+1. Stop before the end.
 
-        # premier +3 pour inclure le codon stop dans la longueur final comme dans ncbi orf finder...
+        # First +3 to include stop codin in final length as in the ncbi orfinder.
         if (pos_stop[i + 1] + 3 - (pos_stop[i] + 3)) < threshold:
             continue
 
@@ -427,8 +373,6 @@ def to_fit_len_max(twoStopLen):
     """
     if twoStopLen <= obj.Gene.length_max:  # if the space between the two stop <= to len max then no need to add anything
         return 0
-    # print 'twoStopLen {} - obj.Gene.length_max {}'.format(twoStopLen, obj.Gene.length_max)
-    # print 'MODULO', (twoStopLen - obj.Gene.length_max) % 3
     return twoStopLen - obj.Gene.length_max
 
 
@@ -436,9 +380,7 @@ def complement_reverse(seq):
     compl_rev_seq = {}
     compl_rev_seq = reverse(seq)
     compl_rev_seq = complement(compl_rev_seq)
-    # print 'In comp rev fct'
     compl_rev_seq['rev'] = not seq['rev']
-    # print 'REV is ...', compl_rev_seq['rev']
     return compl_rev_seq
 
 
@@ -464,32 +406,32 @@ def complement(seq):
     return complement
 
 
-def getGeneticCode(NCBI_ID):
-    """Returns the genetic code corresponding to a given NCBI_ID
+def getGeneticCode(ncbi_genetic_code_id):
+    """Return the genetic code corresponding to a given ncbi_genetic_code_id.
 
     This function is written by Theo Falgarone.
 
     Args:
-        NCBI_ID: NCBI identifier for the genetic code
+        ncbi_genetic_code_id: NCBI identifier for the genetic code
 
     Returns:
         table: genetic code table
 
     """
-    if NCBI_ID == 1:
+    if ncbi_genetic_code_id == 1:
         base1 = "TTTTTTTTTTTTTTTTCCCCCCCCCCCCCCCCAAAAAAAAAAAAAAAAGGGGGGGGGGGGGGGG"
         base2 = "TTTTCCCCAAAAGGGGTTTTCCCCAAAAGGGGTTTTCCCCAAAAGGGGTTTTCCCCAAAAGGGG"
         base3 = "TCAGTCAGTCAGTCAGTCAGTCAGTCAGTCAGTCAGTCAGTCAGTCAGTCAGTCAGTCAGTCAG"
         start = "---M---------------M---------------M----------------------------"
         aas = "FFLLSSSSYY**CC*WLLLLPPPPHHQQRRRRIIIMTTTTNNKKSSRRVVVVAAAADDEEGGGG"
-    elif NCBI_ID == 11:
+    elif ncbi_genetic_code_id == 11:
         base1 = "TTTTTTTTTTTTTTTTCCCCCCCCCCCCCCCCAAAAAAAAAAAAAAAAGGGGGGGGGGGGGGGG"
         base2 = "TTTTCCCCAAAAGGGGTTTTCCCCAAAAGGGGTTTTCCCCAAAAGGGGTTTTCCCCAAAAGGGG"
         base3 = "TCAGTCAGTCAGTCAGTCAGTCAGTCAGTCAGTCAGTCAGTCAGTCAGTCAGTCAGTCAGTCAG"
         start = "---M---------------M------------MMMM---------------M------------"
         aas = "FFLLSSSSYY**CC*WLLLLPPPPHHQQRRRRIIIMTTTTNNKKSSRRVVVVAAAADDEEGGGG"
     else:
-        print('ERROR : votre NCBI_ID ne correspond pas, veuillez entrer 1 ou 11')
+        raise ValueError(f'The given ncbi id {ncbi_genetic_code_id} is not managed yet.')
         return
     table = {}
     codonstart = []
@@ -508,8 +450,7 @@ def getGeneticCode(NCBI_ID):
 
 
 def translate(nucl_seq, codonTable=11):
-    """Returns the translated proteic sequence based on the standard genetic code"""
-
+    """Return the translated proteic sequence based on the standard genetic code."""
     proteic_seq = {}
     codons = getWords(nucl_seq, 3)
 
@@ -528,8 +469,7 @@ def translate(nucl_seq, codonTable=11):
 
 
 def getWords(seq, wlength):
-    """Returns a list of non-overlapping words of a given length in a seq"""
-
+    """Return a list of non-overlapping words of a given length in a seq."""
     seq["data"] = seq["data"].upper()
     words = []
     for i in range(0, len(seq["data"]) - wlength + 1, wlength):
